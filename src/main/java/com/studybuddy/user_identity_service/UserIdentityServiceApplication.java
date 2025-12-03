bbe800782444a8a423572ef27ae6502e08248093
Anantha Krishnan <ananthakrishnanj2001@gmail.com>
2025-12-03 12:16:06 +0530

    @web sec enable

--- src/main/java/com/studybuddy/user_identity_service/UserIdentityServiceApplication.java
+++ src/main/java/com/studybuddy/user_identity_service/UserIdentityServiceApplication.java
@@ -2,8 +2,10 @@ package com.studybuddy.user_identity_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class UserIdentityServiceApplication {
 
 	public static void main(String[] args) {
